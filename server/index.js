fed958caa10e5a6bd50265dc4f53dd4d77cf75a8
prajaktahadole <prajaktahadole@gmail.com>
2023-02-08 11:49:03 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 
-const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config();
+// const cors = require("cors");
+const app = express();
+const bodyParser = require('body-parser')
+//bodyParser provides four express middleware for parsing JSON, Text, URL-encoded
 
 //connect to db 
-
 mongoose.connect(
     process.env.DB_CONNECT,
     {useUnifiedTopology :  true, useNewUrlParser : true},
     () => console.log("connected to db")
 );
 
+//import Routes
+// const dataRoute = require("./routes/data");
+
+// // // Middlewares
+// app.use(cors());
+// app.use(express.json("/data", dataRoute));
+
+
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(express.json())
 
 
 app.listen(4000, ()=>{console.log(
