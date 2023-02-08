039e954a64f0303390e17eea68877322b1e39b21
prajaktahadole <prajaktahadole@gmail.com>
2023-02-08 11:56:14 +0530

    server done

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const DataController = require("../controllers/dataController")
 
-router.post("/", DataController.create_Data);
-router.get("/", DataController.All_Data);
-router.get("/:id", DataController.single_Data);
-router.put("/:id", DataController.Update_Data);
-router.delete("/:id", DataController.Delete_Data);
+router.post("/api/todo/", DataController.create_Data);
+router.get("/api/todo/", DataController.All_Data);
+router.get("/api/todo/:id", DataController.single_Data);
+router.put("/api/todo/:id", DataController.Update_Data);
+router.delete("/api/todo/:id", DataController.Delete_Data);
 
 module.exports = router;
 
